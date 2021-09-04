d9ab4b28a50f450e1f620cc7a79b771650806181
Luis <luisfru96@gmail.com>
2021-09-04 15:33:32 +0200

    Include custom hooks and context

---
+++ src/components/ProductsCard.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react'
+
+import CartContext from '../contexts/CartContext'
+
+const ProductsCard = ({ category, description, id, image, price, rating, title }) => {
+  const { addToCart } = useContext(CartConext)
+
+  const handleAddToCart = product => () => {
+    addToCart(product)
+  }
+
+  return (
+    <div>
+      <img src={image} />
+      <div>
+        <h4>{title}</h4>
+        <span>{rating.rate}</span>
+        <span>{rating.count}</span>
+      </div>
+      <p>{description}</p>
+      <div>{price}</div>
+      <button onClick={handleAddToCart({ category, description, id, image, price, rating, title })}>Add in Cart</button>
+    </div>
+  )
+}
+
+export default ProductsCard
--- src/components/ProductsCardList.jsx
+++ src/components/ProductsCardList.jsx
@@ -1,23 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
-import ProductsCard 
+import ProductsCard from './ProductsCard'
 
 const ProductsCardList = () => {
   const [productsList, setProductsList] = useState([])
-  
+
   useEffect(() => {
-     window.fetch('https://fakestoreapi.com/products')
+    window.fetch('https://fakestoreapi.com/products')
       .then(response => response.json())
       .then(data => setProductsList(data))
   }, [])
 
   return (
     <div>
-    {productsList.map(product => (
-      <ProductsCard key={product.id} {...product}
-    ))}
+      {productsList.map(product => (
+        <ProductsCard key={product.id} {...product} />
+      ))}
     </div>
   )
 }
 
-  export default ProductsCardList
+export default ProductsCardList
